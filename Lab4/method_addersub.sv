2eeb102d9ea866cd2b6836e41b13a97e0a2d81ab
Ignacio Vargas <ignaevc@gmail.com>
2021-10-06 00:28:15 -0600

    Agregada unidad sumador / restadora

---
+++ Lab4/method_addersub.sv
@@ -0,0 +1,20 @@
+module method_addersub#(parameter N=4)(input logic [N-1:0] A, B, 
+												input logic cin, select,
+												output logic [N-1:0] S, 
+												output logic cout);
+
+	logic suma, resta, cout1, cout2;
+
+
+	method_adder #(N) sumador (.a(A), .b(B), .cin(cin), .s(suma), .cout(cout1));
+	method_substractor #(N) restador (.a(A), .b(B), .cin(cin), .s(resta), .cout(cout2));
+	
+	always @(*)
+	begin
+	if(select == 0)
+		{S,cout} = {suma, cout1};
+	if(select == 1)
+		{S,cout} = {resta, cout2};
+	end
+
+endmodule
