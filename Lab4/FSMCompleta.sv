cdd673fdd44ef01506508e31ee498e4be506c39b
Bertha Brenes <bertacaro1996@hotmail.com>
2021-10-11 20:24:04 -0600

    fsm mejorada

--- Lab4/FSMCompleta.sv
+++ Lab4/FSMCompleta.sv
@@ -1,28 +1,30 @@
-module FSMCompleta(input logic clk,rst,x,y,b,to,
-							output logic a,d,c);
+module FSMCompleta(input logic clk,rst,m1,m2,Rc,
+						 input logic [2:0]Sel,
+						 output logic [2:0]boison,
+						 output logic [1:0]sum);
 							
 
 logic [1:0] state, next_state;
 
 // estado actual
 always_ff @(posedge clk or posedge rst)
-	if(rst) state = 3'b000;
+	if(rst) state = 2'b00;
 	else
 		state = next_state;
 
 // siguiente estado
 always_comb
 	case(state)
-	3'b000: if(x) next_state = 3'b010; else begin if(y) next_state =3'b001; else if(b) next_state = 3'b011; else next_state = 3'b000; end
-	3'b001: if(x) next_state = 3'b010; else begin if(y) next_state =3'b001; else if(b) next_state = 3'b011; else next_state = 3'b000; end
-	3'b010: if(x) next_state = 3'b010; else begin if(y) next_state =3'b001; else if(b) next_state = 3'b011; else next_state = 3'b000; end
-	3'b011: if(to) next_state = 3'b100; else next_state = 3'b000;
-	3'b100: next_state = 3'b000;
-	default: next_state = 3'b000;
+	2'b00: if(m1 || m2) next_state = 2'b01; else next_state = 2'b00;
+	2'b01: if(Sel) next_state = 2'b10; else next_state = 2'b00;
+	2'b10: if(Rc) next_state = 2'b11; else next_state = 2'b00;
+	2'b11: next_state = 2'b00;
+	default: next_state = 2'b00;
 	endcase
 	
 // outputs
-assign a = (state == 3'b001);
-assign d = (state == 3'b011);
-assign c = (state == 3'b100);
+assign sum[0] = m1;
+assign sum[1] = m2;
+assign boison = Sel;
+
 endmodule
---
+++ Lab4/FSMCompleta_tb.sv
@@ -0,0 +1,51 @@
+module FSMCompleta_tb();
+	logic clk, reset,x,y,to;
+	logic [2:0] sel;
+	logic [1:0] sum;
+	logic [2:0] bebida;
+	
+	FSMCompleta machine(clk,reset,x,y,to,sel,bebida,sum);
+	always begin
+								
+		#10
+		
+		clk = ~clk;
+			
+	end
+	
+	initial begin
+	
+		clk = 0;
+		reset = 1;		
+		
+		#10
+		
+		reset = 0;
+		x = 1;
+		
+		#10
+		
+		y = 1;
+		
+		#10
+		
+		x = 1;
+		y = 1;
+		
+		#20
+		sel = 3'b001;
+		
+		#20
+		to = 1;
+		#10
+		
+		reset = 1;
+		
+		#10
+		
+		reset = 0;
+	
+	end
+						 
+						 
+endmodule
